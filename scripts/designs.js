9b1b59ebc21981932a5509a2dbaa2a03cfa35043
Traci Williams <ug3000@gmail.com>
2017-10-11 13:07:42 -0500

    making grid function

--- scripts/designs.js
+++ scripts/designs.js
@@ -1,10 +1,45 @@
-// Select color input
-// Select size input
+//testing to make sure files are connected
+/*window.onload = function(){
+	console.log("JS is working")
+};
 
+
+$(document).ready(function(){
+	$('body').on('click', function(){
+		$(this).toggleClass('clicked');
+		console.log('clicked');
+	});
+});
+*/
+
+// Select color input
+//var colorPicker = document.getElementById("colorPicker").value;
+// Select size input
+//var gridHeight = document.getElementById("input_height").value;
+//var gridWidth = document.getElementById("input_width").value;
+//var tableBody = "";
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
 
-// Your code goes here!
+	let gridHeight = document.getElementById('input_height').value;
+	let gridWidth = document.getElementById('input_width').value;
+	let tableHeader = '<table border="1">\n';
+	let tableBody = '';
+	
 
-}
+	let colorPicker = document.getElementById('colorPicker').value;
+
+	for (var i = 0; i < gridHeight; i++) {
+		
+		tableBody += '<tr>';
+		for (var j = 0; j < gridWidth; j++) {
+			tableBody += '<td>'; 
+			tableBody += 'Cell' + i + ',' + j;
+			tableBody += '</td>';
+		}
+		tableBody += '</tr>\n';
+	}
+	let tableFooter = '</table>';
+	document.getElementById('pixel_canvas').innerHTML = tableHeader + tableBody + tableFooter;
+};
